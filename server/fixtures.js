c71984578d41eb8c5e8378397b52373c4e79d061
Dicky <richard.legg.89@gmail.com>
2015-11-11 21:22:57 +1300

    linked collection into postsList template

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,6 +1,6 @@
-if (Posts.find().count === 0){
-	Posts.insert({
-		title: 'Introducing Telescope',
+if (Posts.find().count() === 0) {
+  Posts.insert({
+    title: 'Introducing Telescope',
     url: 'http://sachagreif.com/introducing-telescope/'
   });
 
