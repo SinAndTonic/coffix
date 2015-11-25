73accb0616eb56ab21bbabba2790ad539e27af97
Dicky <richard.legg.89@gmail.com>
2015-11-25 17:30:34 +1300

    added view extras and usernames to orders

--- client/templates/orders/place_order.js
+++ client/templates/orders/place_order.js
@@ -2,12 +2,15 @@ Template.placeOrder.events({
 	'submit form': function(e){
 		e.preventDefault();
 
-		var theExtras = {
-			sugar: $(e.target).find('[name=sugar]').is(":checked"),
-			almond: $(e.target).find('[name=almond]').is(":checked")
-		};
+		var userId = Meteor.userId();
+
+		var userName = Meteor.user().username;
+
+		var theExtras = ['Sugar','Syrup'];
 
 		var order = {
+			UserId: userId,
+			userName: userName,
 			type: $(e.target).find('[name=type]').val(),
 			milk: $(e.target).find('[name=milk]').val(),
 			extras: theExtras
@@ -19,7 +22,9 @@ Template.placeOrder.events({
 		//};
 		//console.log(post);
 		//console.log(extras);
+		console.log(order);
 		order._id = Orders.insert(order);
 		//Router.go('postPage', post);
+		//Meteor.call('removeOrder', this._id);
 	}
 });
