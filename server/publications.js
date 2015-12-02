d3d5523362c558cae21a633046c0f10fe558db6a
Dicky <richard.legg.89@gmail.com>
2015-12-02 21:09:25 +1300

    finished adding user roles, and admin on startup

--- server/publications.js
+++ server/publications.js
@@ -1,16 +1,16 @@
-Meteor.publish('orders', function() {
-	return Orders.find();
-});
+//Meteor.publish('orders', function() {
+//	return Orders.find();
+//});
 
 Meteor.publish('itemNames', function() {
 	return ItemNames.find();
 });
 
-//Meteor.publish( 'orders', function() {
-//  if ( Roles.userIsInRole( this.userId, 'admin') ) {
-//    return Orders.find();
-//  } else {
-//    this.stop();
-//    return;
-//  }
-//});
+Meteor.publish( 'orders', function() {
+  if ( Roles.userIsInRole( this.userId, 'admin') ) {
+    return Orders.find();
+  } else {
+    this.stop();
+    return;
+  }
+});
--- server/users.js
+++ server/users.js
@@ -1,2 +1,31 @@
-Roles.addUsersToRoles( 'eEGzvyDuRMrKbmPrv', ['admin']);
-console.log('added user roles');
+Meteor.startup(function(){
+	if(Meteor.users.find().count() < 1){
+
+		var users = [
+
+		      {name:"admin",email:"admin@example.com",roles:['admin']}
+
+		    ];
+
+		_.each(users, function (user) {
+		  var id;
+
+		  id = Accounts.createUser({
+		  	username: user.name,
+		    email: user.email,
+		    password: "12345678",
+		    profile: { name: user.name }
+		  });
+
+		  if (user.roles.length > 0) {
+		    // Need _id of existing user record so this call must come 
+		    // after `Accounts.createUser` or `Accounts.onCreate`
+		    Roles.addUsersToRoles(id, user.roles, 'admin');
+		  }
+
+		});
+
+	}
+});
+
+
