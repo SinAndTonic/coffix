629ebe2d0cf7ac86da3777157cccf05969e72bc2
Dicky <richard.legg.89@gmail.com>
2015-12-03 16:59:24 +1300

    fixed autoform for items

--- server/security.js
+++ server/security.js
@@ -3,5 +3,3 @@ Orders.permit(['remove','update','insert']).ifHasRole('admin').apply();
 Orders.permit('insert').ifLoggedIn().apply();
 
 ItemNames.permit(['remove','insert','update']).ifHasRole('admin').apply();
-
-Books.permit(['remove','insert','update']).ifLoggedIn().apply();
