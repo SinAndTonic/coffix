2b88089163de52df011bfb30acb32a022b92604b
Dicky <richard.legg.89@gmail.com>
2015-12-03 17:48:51 +1300

    added basic autoform for ordering

--- client/templates/orders/place_order.js
+++ client/templates/orders/place_order.js
@@ -1,43 +1,43 @@
-Template.placeOrder.events({
-	'submit form': function(e){
-		e.preventDefault();
+// Template.placeOrder.events({
+// 	'submit form': function(e){
+// 		e.preventDefault();
 
-		var userId = Meteor.userId();
+// 		var userId = Meteor.userId();
 
-		var userName = Meteor.user().username;
+// 		var userName = Meteor.user().username;
         
-     var theExtras = [];
+//      var theExtras = [];
 
-     $('#extraArea :checked').each(function() {
-       theExtras.push($(this).val());
-     });
+//      $('#extraArea :checked').each(function() {
+//        theExtras.push($(this).val());
+//      });
 
-     var theOrder = [];
+//      var theOrder = [];
 
-	$('#orderArea option:selected').each(function() {
-       theOrder.push($(this).val());
-     });
+// 	$('#orderArea option:selected').each(function() {
+//        theOrder.push($(this).val());
+//      });
 
 
-		var order = {
-			userId: userId,
-			userName: userName,
-			createdAt: new Date(),
-			theOrder: theOrder,
-			extras: theExtras,
-			complete: false
-		};
+// 		var order = {
+// 			userId: userId,
+// 			userName: userName,
+// 			createdAt: new Date(),
+// 			theOrder: theOrder,
+// 			extras: theExtras,
+// 			complete: false
+// 		};
 
-		//var totalOrder = {
-		//	mainOrder: order,
-		//	extras: extras
-		//};
-		//console.log(post);
-		//console.log(extras);
-		//console.log(order);
-		order._id = Orders.insert(order);
-		//Router.go('postPage', post);
-		//Meteor.call('newOrder', order);
-	}
+// 		//var totalOrder = {
+// 		//	mainOrder: order,
+// 		//	extras: extras
+// 		//};
+// 		//console.log(post);
+// 		//console.log(extras);
+// 		//console.log(order);
+// 		order._id = Orders.insert(order);
+// 		//Router.go('postPage', post);
+// 		//Meteor.call('newOrder', order);
+// 	}
 
-});
+// });
