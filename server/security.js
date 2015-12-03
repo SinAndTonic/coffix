9939b1c858d46d00593e366bce466c19f03fc9b1
Dicky <richard.legg.89@gmail.com>
2015-12-03 13:57:17 +1300

    added admin only navbar

---
+++ server/security.js
@@ -0,0 +1,5 @@
+Orders.permit(['remove','update','insert']).ifHasRole('admin').apply();
+
+Orders.permit('insert').ifLoggedIn().apply();
+
+ItemNames.permit(['remove','insert','update']).ifHasRole('admin').apply();
